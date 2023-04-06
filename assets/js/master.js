b73078d8fbcb9485ad3198950c8d3a9892006bd3
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2023-04-06 16:04:57 +0200

    docs: added pouap cookies info

--- assets/js/master.js
+++ assets/js/master.js
@@ -688,3 +688,43 @@
 	});
 
 /*End Jquery*/
+
+
+/* Kod JavaScript dla popupa z informacją o plikach cookie */
+// funkcja, która wyświetla popup z informacją o plikach cookie
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function acceptCookies() {
+  if (document.getElementById("accept-cookies").checked) {
+    setCookie("cookiesAccepted", "true", 31);
+  }
+  document.getElementById("cookie-popup").style.display = "none";
+}
+
+if (!getCookie("cookiesAccepted")) {
+  document.getElementById("cookie-popup").style.display = "block";
+}
+
+
+
+
+
